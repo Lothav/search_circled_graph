d312942abdf24e0539f63d0e32636d4c20837d79
Luiz0tavio <luizotavioresende@hotmail.com>
2016-05-31 22:40:21 -0300

    opt

--- main.c
+++ main.c
@@ -30,8 +30,8 @@ typedef struct {
 } City;
 
 int minDistance(int dist[], bool sptSet[], int n) {
-    int min = INT_MAX, min_index;
 
+    int min = INT_MAX, min_index;
     min_index = 0;
     for (int v = 0; v < n; v++)
         if (sptSet[v] == false && dist[v] <= min)
@@ -41,27 +41,21 @@ int minDistance(int dist[], bool sptSet[], int n) {
 }
 
 void dijkstra(int **graph, int n, int src, int to) {
-    int dist[n];
 
-    bool sptSet[n];
+    int dist[n], sptSet[n];
 
     for (int i = 0; i < n; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
 
     dist[src] = 0;
-
     for (int count = 0; count < n-1; count++) {
-
         int u = minDistance(dist, sptSet, n);
-
         sptSet[u] = true;
-
         for (int v = 0; v < n; v++)
             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
                 && dist[u]+graph[u][v] < dist[v])
-                dist[v] = dist[u] + (graph[u][v] > 1 ? graph[u][v] : 0);
+                dist[v] = dist[u] + graph[u][v];
     }
-
     printf("%d\n", dist[to] == INT_MAX ? -1 : dist[to]);
 }
 
@@ -72,7 +66,7 @@ int getPos(int x, int y, int max_x, int max_y){
 
 int getAdjMatCost(City *cit, int i, int j, int rest_x, int rest_y, int offsetx) {
 
-    int b, a, t, sig_x, sig_y, block_a, block_b, index, last_index;
+    int b, a, t, sig_x, sig_y, index, last_index;
     int cost_x, cost_y;
 
     if(cit[i].cost == -1 && cit[j].cost == -1) return 1;
@@ -97,81 +91,65 @@ int getAdjMatCost(City *cit, int i, int j, int rest_x, int rest_y, int offsetx) 
                 continue;
 
         if( (i + (rest_x * sig_x) + (sig_y*offsetx*rest_y)) == j ){
-
-            cost_x = block_a = block_b = index = false;
-            last_index = sig_x + i;
+            // cost x
+            cost_x = index = false;
+            last_index = i;
 
             for(a = 1; a <= rest_x; a++){
-
                 index = sig_x*a + i;
-
-                if(cit[index].pos == pos_edge_left) block_a = true;
-                if(cit[index].pos == pos_edge_right) block_b = true;
-
-                if(!cit[index].cost || cit[index].pos == pos_edge_left && block_b || cit[index].pos == pos_edge_right && block_a){
+                if(!cit[index].cost ||
+                    ((cit[index].pos != pos_edge_top && cit[index].pos != pos_center
+                    && cit[index].pos != pos_edge_bottom) && (a < rest_x))){
                     cost_x = 0;
                     break;
                 }
-
                 cost_x += (cit[index].cost + cit[last_index].cost);
                 last_index = index;
             }
 
-            block_a = block_b = false;
             if(cost_x){
                 for(b = 1; b <= rest_y; b++){
-
                     index += sig_y*offsetx;
-
-                    if(cit[index].pos == pos_edge_top) block_a = true;
-                    if(cit[index].pos == pos_edge_bottom) block_b = true;
-
-                    if(!cit[index].cost || cit[index].pos == pos_edge_top && block_b || cit[index].pos == pos_edge_bottom && block_a){
+                    if(!cit[index].cost ||
+                       ((cit[index].pos != pos_center && cit[index].pos != pos_edge_left &&
+                         cit[index].pos != pos_edge_right)&& (a < rest_y))){
                         cost_x = 0;
                         break;
                     }
-
                     cost_x += (cit[index].cost + cit[last_index].cost);
                     last_index = index;
                 }
             }
 
+
             // cost y
-            block_a = block_b = cost_y = index = false;
-            last_index = i + (sig_y*offsetx);
+             cost_y = index = false;
+            last_index = i;
             for(a = 1; a <= rest_y; a++){
-
                 index = i + (sig_y*a*offsetx);
-                if(cit[index].pos == pos_edge_top) block_a = true;
-                if(cit[index].pos == pos_edge_bottom) block_b = true;
-
-                if(!cit[index].cost || cit[index].pos == pos_edge_top && block_b || cit[index].pos == pos_edge_bottom && block_a){
+                if(!cit[index].cost ||
+                ((cit[index].pos != pos_center && cit[index].pos != pos_edge_left &&
+                        cit[index].pos != pos_edge_right)&& (a < rest_y))){
                     cost_y = 0;
                     break;
                 }
-
                 cost_y += (cit[index].cost + cit[last_index].cost);
                 last_index = index;
             }
-            block_a = false;
-            block_b = false;
-
             if(cost_y){
                 for(b = 1; b <= rest_x; b++) {
-
                     index += sig_x;
-                    if(cit[index].pos == pos_edge_left) block_a = true;
-                    if(cit[index].pos == pos_edge_right) block_b = true;
-
-                    if(!cit[index].pos || cit[index].pos == pos_edge_top && block_b || cit[index].pos == pos_edge_bottom && block_a){
-                        cost_y = 0;
-                        break;
+                    if(!cit[index].cost ||
+                     ((cit[index].pos != pos_edge_top && cit[index].pos != pos_center &&
+                       cit[index].pos != pos_edge_bottom) && (b < rest_x))){
+                            cost_y = 0;
+                            break;
                     }
-
                     cost_y += (cit[index].cost + cit[last_index].cost);
                     last_index = index;
                 }
             }
+
             return !cost_x ? cost_y : !cost_y ? cost_x : (cost_x > cost_y) ? cost_y : cost_x;
         }
     }
@@ -214,32 +192,12 @@ int main(int* argc, char* argv[]) {
             adj_matrix[i][j] = adj_matrix[j][i] = i == j ? 0 : getAdjMatCost(cit, i, j, atoi(argv[6]), atoi(argv[7]), size_x);
         }
 
-    for(i = 0; i < size_adj_matrix; i++) {
-        for(j = 0; j < size_adj_matrix; j++)
-            printf("%d\t",adj_matrix[i][j]);
-        printf("\t %d \n", i);
-    }
-    printf("\n");
-    for(i = 0; i < size_adj_matrix; i++) {
-        printf("%d\t",i);
-    }
-    printf("\n\n");
     dijkstra(adj_matrix, size_adj_matrix, from, to);
 
-    return 0;
+    free(cit);
+    for(i = 0; i <  size_adj_matrix; i++) free(adj_matrix[i]);
+    free(adj_matrix);
+    fclose(map);
+
+    return EXIT_SUCCESS;
 }
-
-//     0 1 4 3 2 1
-
-/*
-
- 0   1  2  3   4   5
-
-198    0   0  35  20   14   5
-|232|  0   0  14  21   13   11
-147    72 61  66  33   16   17
-87    78  81 141 |130| 40   23
-
- *
- *
- * */
---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+time ./tp2 testA.txt 0 0 99 99 0 0 > outputs.txt
+time ./tp2 testA.txt 0 0 99 99 1 1 >> outputs.txt
+time ./tp2 testA.txt 0 0 99 99 1 2 >> outputs.txt
+time ./tp2 testA.txt 0 0 99 99 2 3 >> outputs.txt
+
+time ./tp2 testB.txt 0 0 99 99 0 0 >> outputs.txt
+time ./tp2 testB.txt 0 0 99 99 1 1 >> outputs.txt
+time ./tp2 testB.txt 0 0 99 99 1 2 >> outputs.txt
+time ./tp2 testB.txt 0 0 99 99 2 3 >> outputs.txt
+
+time ./tp2 testC.txt 0 0 199 199 0 0 >> outputs.txt
+time ./tp2 testC.txt 0 0 199 199 1 1 >> outputs.txt
+time ./tp2 testC.txt 0 0 199 199 1 2 >> outputs.txt
+time ./tp2 testC.txt 0 0 199 199 2 3 >> outputs.txt
+
+time ./tp2 testD.txt 0 0 199 199 0 0 >> outputs.txt
+time ./tp2 testD.txt 0 0 199 199 1 1 >> outputs.txt
+time ./tp2 testD.txt 0 0 199 199 1 2 >> outputs.txt
+time ./tp2 testD.txt 0 0 199 199 2 3 >> outputs.txt
+
+time ./tp2 testE.txt 0 0 299 299 0 0 >> outputs.txt
+time ./tp2 testE.txt 0 0 299 299 1 1 >> outputs.txt
+time ./tp2 testE.txt 0 0 299 299 1 2 >> outputs.txt
+time ./tp2 testE.txt 0 0 299 299 2 3 >> outputs.txt
+
+time ./tp2 testF.txt 0 0 299 299 0 0 >> outputs.txt
+time ./tp2 testF.txt 0 0 299 299 1 1 >> outputs.txt
+time ./tp2 testF.txt 0 0 299 299 1 2 >> outputs.txt
+time ./tp2 testF.txt 0 0 299 299 2 3 >> outputs.txt
